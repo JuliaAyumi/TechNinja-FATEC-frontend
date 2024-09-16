b42108a2d1d038888a0b99e67a3f8bf05fc127e4
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-09-16 15:42:26 -0300

    feat: nova estrutura de pastas

--- app/server/src/config/db.js
+++ app/server/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      ""
+      "mongodb+srv://admbancotechninja:OLc8MPqLwxmyEG34@techninja.jrs23.mongodb.net/TechNinja?retryWrites=true&w=majority"
     );
     console.log("Conectado ao MongoDB Atlas");
   } catch (error) {
