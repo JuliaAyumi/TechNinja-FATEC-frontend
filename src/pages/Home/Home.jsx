4b46a4f4c31f39784e196778b71d9c71bbe51110
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-09-25 07:23:23 -0300

    feat: criando rotas dinamicas para cada area de conhecimento

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,49 +1,45 @@
 import { Link } from "react-router-dom";
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import "./Home.css";
-// import AreaConhecimentoCard from "../../components/AreaConhecimentoCard/AreaConhecimentoCard";
 
 const Home = () => {
+  const areas = [
+    {
+      name: "historia-tecnologia",
+      title: "Sistemas Operacionais",
+      icon: "icon-sistemas-operacionais.svg",
+    },
+    {
+      name: "linguagem-programacao",
+      title: "Linguagem de Programação",
+      icon: "icon-linguagem-de-programacao.svg",
+    },
+    {
+      name: "modelagem-dados",
+      title: "Modelagem de Dados",
+      icon: "icon-modelagem-de-dados.svg",
+    },
+  ];
+
   return (
     <div>
       <HeaderHome />
-
       <main className="main-home">
-        {/* <AreaConhecimentoCard
-          title="Sistemas Operacionais"
-          icon="src/assets/icons/icon-sistemas-operacionais.svg"
-          link="/quizzes"
-        /> */}
-
-        <Link to="/quizzes" className="area-conhecimento">
-          <div id="ac1" onClick="window.location.href='../Quizes/quizes.html'">
-            <h1>Sistemas Operacionais</h1>
-            <img
-              src="src/assets/icons/icon-sistemas-operacionais.svg"
-              alt="Ícone de uma computador para ir para a área de conhecimento de Sistemas Operacionais"
-            />
-          </div>
-        </Link>
-
-        <Link to="/quizzes" className="area-conhecimento">
-          <div id="ac2" onClick="window.location.href='../Quizes/quizes.html'">
-            <h1>Linguagem de Programação</h1>
-            <img
-              src="src/assets/icons/icon-linguagem-de-programacao.svg"
-              alt="Ícone de um código para ir para a área de conhecimento de Linguagem de Programação"
-            />
-          </div>
-        </Link>
-
-        <Link to="/quizzes" className="area-conhecimento">
-          <div id="ac3" onClick="window.location.href='../Quizes/quizes.html'">
-            <h1>Modelagem de Dados</h1>
-            <img
-              src="src/assets/icons/icon-modelagem-de-dados.svg"
-              alt="Ícone de um banco de dados para ir para a área de conhecimento de Modelagem de Dados"
-            />
-          </div>
-        </Link>
+        {areas.map((area) => (
+          <Link
+            key={area.name}
+            to={`/quizzes/${area.name}`}
+            className="area-conhecimento"
+          >
+            <div id={`ac-${area.name}`}>
+              <h1>{area.title}</h1>
+              <img
+                src={`src/assets/icons/${area.icon}`}
+                alt={`Ícone da área de conhecimento de ${area.title}`}
+              />
+            </div>
+          </Link>
+        ))}
       </main>
     </div>
   );
