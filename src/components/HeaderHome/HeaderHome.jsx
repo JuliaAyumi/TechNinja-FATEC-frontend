309489b5b4159efc04fc53943e2bbeb670175992
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-10-20 18:19:12 -0300

    feat: icon de modo escuro e modo claro com icons dinâmicos

--- src/components/HeaderHome/HeaderHome.jsx
+++ src/components/HeaderHome/HeaderHome.jsx
@@ -1,22 +1,35 @@
 import { Link } from "react-router-dom";
 import "./HeaderHome.css";
+import { useState, useEffect } from "react";
 import iconTutorial from "../../assets/icons/icon-tutorial.png";
 import iconPerfil from "../../assets/icons/icon-profile-white.png";
 import iconDarkmode from "../../assets/icons/icon-darkmode-white.png";
-import iconConfig from "../../assets/icons/icon-configuracoes.png"
+import iconLightmode from "../../assets/icons/icon-claro.png";
+import iconConfig from "../../assets/icons/icon-configuracoes.png";
 
 const HeaderHome = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   const toggleDarkMode = () => {
     const currentMode = localStorage.getItem("accessibilityMode");
     if (currentMode === "dark-mode") {
       localStorage.setItem("accessibilityMode", "light-mode");
       document.body.classList.remove("dark-mode");
+      setIsDarkMode(false);
     } else {
       localStorage.setItem("accessibilityMode", "dark-mode");
       document.body.classList.add("dark-mode");
+      setIsDarkMode(true);
     }
   };
-  
+
+  useEffect(() => {
+    const currentMode = localStorage.getItem("accessibilityMode");
+    if (currentMode === "dark-mode") {
+      setIsDarkMode(true);
+    }
+  }, []);
+
   return (
     <header className="header-home">
       <h3>TECHNINJA</h3>
@@ -32,20 +45,21 @@ const HeaderHome = () => {
           </a>
 
           <Link to="/perfil">
-            <img
-              src={iconPerfil}
-              title="Perfil"
-              alt="Icone Meu Perfil"
-            />
+            <img src={iconPerfil} title="Perfil" alt="Icone Meu Perfil" />
           </Link>
 
           <img
-            src={iconDarkmode}
-            title="Modo escuro"
-            alt="Icone de uma lua para ativar modo escuro"
+            src={isDarkMode ? iconLightmode : iconDarkmode}
+            title={isDarkMode ? "Modo claro" : "Modo escuro"}
+            alt={
+              isDarkMode
+                ? "Ícone de sol para ativar modo claro"
+                : "Ícone de lua para ativar modo escuro"
+            }
             id="modoescuro"
             onClick={toggleDarkMode}
           />
+
           <Link to="/configuracoes">
             <img
               src={iconConfig}
