717a38b34ff1064e12507d16268dba815f828d46
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-10-20 18:18:49 -0300

    fix: icons das áreas de conhecimento não aparecendo em produção

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
+import IconLinguagemProgramacao from "../../assets/icons/icon-linguagem-de-programacao.svg";
+import IconSistemasOperacionais from "../../assets/icons/icon-sistemas-operacionais.svg";
+import IconModelagemDados from "../../assets/icons/icon-modelagem-de-dados.svg";
 import "./Home.css";
 
 const Home = () => {
@@ -7,17 +10,17 @@ const Home = () => {
     {
       name: "linguagem-programacao",
       title: "Linguagem de Programação",
-      icon: "icon-linguagem-de-programacao.svg",
+      icon: IconLinguagemProgramacao,
     },
     {
       name: "logica-programacao",
       title: "Lógica de Programação",
-      icon: "icon-sistemas-operacionais.svg",
+      icon: IconSistemasOperacionais,
     },
     {
       name: "modelagem-dados",
       title: "Modelagem de Dados",
-      icon: "icon-modelagem-de-dados.svg",
+      icon: IconModelagemDados,
     },
   ];
 
@@ -34,7 +37,7 @@ const Home = () => {
             <div id={`ac-${area.name}`}>
               <h1>{area.title}</h1>
               <img
-                src={`src/assets/icons/${area.icon}`}
+                src={area.icon}
                 alt={`Ícone da área de conhecimento de ${area.title}`}
               />
             </div>
